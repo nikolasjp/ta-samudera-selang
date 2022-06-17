30af11671b87c0d9a38b18991578aff4a197169d
NicholassJP <nicholass.j.purnama@sat.co.id>
2022-06-17 20:34:22 +0700

    updated

--- routes/web.php
+++ routes/web.php
@@ -70,7 +70,3 @@ Route::get('/produk_felxible', [UserController::class, 'produk_felxible']);
 Route::get('/produk_assesoris', [UserController::class, 'produk_assesoris']);
 Route::get('/produk_pipa_nozel', [UserController::class, 'produk_pipa_nozel']);
 Route::get('/produk_industrial', [UserController::class, 'produk_industrial']);
-
-Route::get('/linkstorage', function () {
-    Artisan::call('storage:link');
-});
